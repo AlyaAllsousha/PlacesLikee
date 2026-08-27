91fc37c71213ecb1bc8d39e526c5b7ea90d84ef9
AlyaAllsousha <albisharafikova@yandex.ru>
2026-08-27 19:34:58 +0300

    Notification

--- app/src/main/java/com/example/placeslikee/presentation/MainActivity.kt
+++ app/src/main/java/com/example/placeslikee/presentation/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.placeslikee.presentation
 
+import android.app.NotificationManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.placeslikee.ui.theme.PlacesLikeeTheme
 
@@ -16,20 +19,39 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private var navController: NavHostController? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PlacesLikeeTheme {
-                val navController = rememberNavController()
+            val navHostController = rememberNavController()
+            navController = navHostController
 
+            PlacesLikeeTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    bottomBar = { BottomNavigationBar(navController = navController) },
+                    bottomBar = { BottomNavigationBar(navController = navHostController) },
                     content = { padding ->
-                        NavHostContainer(navController = navController, padding = padding) }
+                        NavHostContainer(navController = navHostController, padding = padding) }
                 )
             }
+        }
+        intent?.data?.lastPathSegment?.let { markerId ->
+            val notificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.cancel(markerId.hashCode())
+        }
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        navController?.handleDeepLink(intent)
+        val markerId = intent.data?.lastPathSegment
+        if (markerId != null) {
+            val notificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.cancel(markerId.hashCode())
         }
     }
 }
